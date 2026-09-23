62d660b74dd41e466a6f6ff5fba8107b4dd9c9d8
gumpertsport123-glitch <gumpertsport123@gmail.com>
2026-09-23 12:34:19 +0300

    Добавил возведение в степень

--- src/calculator/core.py
+++ src/calculator/core.py
@@ -14,3 +14,5 @@ def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Деление на ноль невозможно")
     return a / b
+def power(a: float, b: float) -> float:
+    return a ** b
